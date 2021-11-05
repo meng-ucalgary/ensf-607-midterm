6686cd74892793f1a5c5fa147980441936a9cda5
Bhavyai Gupta <zbhavyai@gmail.com>
2021-11-05 13:07:32 -0600

    Initial commit

---
+++ StringModel.java
@@ -0,0 +1,15 @@
+public class StringModel {
+    private String myString;
+
+    public void concatenate(String s1, String s2) {
+        setMyString(s1 + s2);
+    }
+
+    public String getMyString() {
+        return myString;
+    }
+
+    public void setMyString(String myString) {
+        this.myString = myString;
+    }
+}
